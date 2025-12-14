ebca04b36c27a12e1338cb3691fa674818467974
shaokiat <shaokiat@gmail.com>
2025-12-14 20:53:54 +0800

    add assetPrefix

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,7 @@ const isProd = process.env.NODE_ENV === "production";
 const nextConfig: NextConfig = {
   output: "export",
   basePath: isProd ? "/shaokiat-portfolio" : "",
+  assetPrefix: isProd ? "/shaokiat-portfolio/" : "",
   images: {
     unoptimized: true,
   },
