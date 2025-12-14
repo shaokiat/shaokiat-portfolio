a27a95c4b221f8a3419855c98f50cf5bad54929d
shaokiat <shaokiat@gmail.com>
2025-12-14 21:02:32 +0800

    chore: Remove `assetPrefix` from Next.js config and disable the `Setup Pages` GitHub Actions step.

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,6 @@ const isProd = process.env.NODE_ENV === "production";
 const nextConfig: NextConfig = {
   output: "export",
   basePath: isProd ? "/shaokiat-portfolio" : "",
-  assetPrefix: isProd ? "/shaokiat-portfolio/" : "",
   images: {
     unoptimized: true,
   },
