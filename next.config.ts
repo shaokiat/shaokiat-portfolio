ecbfb1cdf8d0a9b1b43a471cf35d029740911d9e
shaokiat <shaokiat@gmail.com>
2025-12-14 20:48:57 +0800

    update basePath

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,10 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: '/shaokiat-portfolio',
-  assetPrefix: '/shaokiat-portfolio/',
+  basePath: isProd ? "/shaokiat-portfolio" : "",
   images: {
     unoptimized: true,
   },
